ac8fd70aefbd36e1f3171cee10eb39cd72c4cb67
Amine-bc <lm_bouchoucha@esi.dz>
2023-02-22 21:34:06 +0100

    Fisrt commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // System.out.println("Hello world!")  ;
+        // Data_types.Dtypes()  ;
+        // Input.Scan()  ;
+        // Conditional_switch.condition()  ;
+        // Conditional_switch.switcher();
+        //Arrays_.Arrays_manip();
+        Loop.for_while();
+    }
+}
